0e60da39b02c5b9f59efaa7617a3181de336a104
Antia <antialopezgarcia@gmail.com>
2023-04-18 09:45:09 +0200

    create categories view holder

---
+++ app/src/main/java/com/example/taskmanager/CategoriesViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.taskmanager
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoriesViewHolder(view:View): RecyclerView.ViewHolder(view) {
+
+
+}
