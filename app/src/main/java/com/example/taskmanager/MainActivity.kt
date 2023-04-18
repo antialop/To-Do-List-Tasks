c9301883eb8f78d4ffbbb952a856c3aa7d3c187f
Antia <antialopezgarcia@gmail.com>
2023-04-18 09:48:33 +0200

    init components mainActivity

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.taskmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvCategories: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponent()
+        initUI()
+    }
+
+    private fun initComponent() {
+        rvCategories = findViewById(R.id.rvCategories)
+    }
+    private fun initUI(){
+
     }
 }
