da60769994ab0e69ecced04dd14482854175a573
Antia <antialopezgarcia@gmail.com>
2023-04-18 12:38:30 +0200

    fix list task

--- app/src/main/java/com/example/taskmanager/TaskAdapter.kt
+++ app/src/main/java/com/example/taskmanager/TaskAdapter.kt
@@ -3,15 +3,13 @@ package com.example.taskmanager
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TaskAdapter(private val task: Task) :
+class TaskAdapter(private val tasks: List<Task>) :
     RecyclerView.Adapter<TaskViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = tasks.size
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         TODO("Not yet implemented")
