61cf49cba362afaf93fa117621384ab5e4c27a07
Antia <antialopezgarcia@gmail.com>
2023-04-18 12:36:48 +0200

    implement members in adapter

--- app/src/main/java/com/example/taskmanager/TaskAdapter.kt
+++ app/src/main/java/com/example/taskmanager/TaskAdapter.kt
@@ -1,4 +1,19 @@
 package com.example.taskmanager
 
-class TaskAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskAdapter(private val task: Task) :
+    RecyclerView.Adapter<TaskViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
