651740d58baabec1235370fe747849f4ac766075
Antia <antialopezgarcia@gmail.com>
2023-04-18 11:54:33 +0200

    add color divider

--- app/src/main/java/com/example/taskmanager/CategoriesViewHolder.kt
+++ app/src/main/java/com/example/taskmanager/CategoriesViewHolder.kt
@@ -1,7 +1,9 @@
 package com.example.taskmanager
 
+import android.content.ContextParams
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -13,12 +15,22 @@ class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         when(taskCategory){
             TaskCategory.Business -> {
                 tvCategoryName.text = "Negocios"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context,R.color.business_category)
+                )
+
             }
             TaskCategory.Other -> {
                 tvCategoryName.text = "Otros"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context,R.color.other_category)
+                )
             }
             TaskCategory.Personal -> {
                 tvCategoryName.text = "Personal"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context,R.color.personal_category)
+                )
             }
         }
     }
